2385356f21f5706ecdcf9a78f3ebe46699df8b92
Evgeniy <e.pogrebnyak@gmail.com>
2020-08-05 12:24:08 +0300

    pep8

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
-from friends import Friends, Comments, Posts, AddressBook 
+from friends import Friends, Comments, Posts, AddressBook
 
 f = Friends("./facebook-epogrebnyak")
-friends = f.get_tuples()         # list of (timestamp, name) tuples
-friends_dicts = f.get_dicts()    # same data as list of dictionaries
-friends_gen = f.iterate()        # useful for streaming large archives
-friends_df = f.get_dataframe()   # pandas DataFrame ready for analysis 
-f.save_csv("./output_folder")    # saves data to 'friends.csv'
+friends = f.get_tuples()  # list of (timestamp, name) tuples
+friends_dicts = f.get_dicts()  # same data as list of dictionaries
+friends_gen = f.iterate()  # useful for streaming large archives
+friends_df = f.get_dataframe()  # pandas DataFrame ready for analysis
+f.save_csv("./output_folder")  # saves data to 'friends.csv'
